83efc3b822e7dfa28430a063f690cbb8ac8f822e
Louis Marc Leonard <llouismrc2@gmail.com>
2024-09-13 12:12:42 -0400

    sticky note

--- lib/src/feature/notes/sticky_note_ui.dart
+++ lib/src/feature/notes/sticky_note_ui.dart
@@ -7,7 +7,6 @@ class StickyNote extends StatelessWidget {
       {super.key, required this.child, this.color = const Color(0xffffff00)});
 
   final Widget child;
-
   final Color color;
 
   @override
@@ -29,59 +28,45 @@ class StickyNotePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     _drawShadow(size, canvas);
-
     Paint gradientPaint = _createGradientPaint(size);
-
     _drawNote(size, canvas, gradientPaint);
   }
 
   void _drawNote(Size size, Canvas canvas, Paint gradientPaint) {
-    Path path = new Path();
-
+    Path path = Path();
     path.moveTo(0, 0);
-
     path.lineTo(size.width, 0);
-
     path.lineTo(size.width, size.height);
 
     double foldAmount = 0.12;
-
     path.lineTo(size.width * 3 / 4, size.height);
 
     path.quadraticBezierTo(size.width * foldAmount * 2, size.height,
         size.width * foldAmount, size.height - (size.height * foldAmount));
-
     path.quadraticBezierTo(
         0, size.height - (size.height * foldAmount * 1.5), 0, size.height / 4);
-
     path.lineTo(0, 0);
 
     canvas.drawPath(path, gradientPaint);
   }
 
   Paint _createGradientPaint(Size size) {
-    Paint paint = new Paint();
+    Paint paint = Paint();
 
     Rect rect = Rect.fromLTWH(0, 0, size.width, size.height);
-
-    RadialGradient gradient = new RadialGradient(
+    RadialGradient gradient = RadialGradient(
         colors: [brighten(color), color],
         radius: 1.0,
         stops: const [0.5, 1.0],
         center: Alignment.bottomLeft);
-
     paint.shader = gradient.createShader(rect);
-
     return paint;
   }
 
   void _drawShadow(Size size, Canvas canvas) {
     Rect rect = Rect.fromLTWH(12, 12, size.width - 24, size.height - 24);
-
-    Path path = new Path();
-
+    Path path = Path();
     path.addRect(rect);
-
     canvas.drawShadow(path, Colors.black.withOpacity(0.7), 12.0, true);
   }
 
@@ -93,7 +78,6 @@ class StickyNotePainter extends CustomPainter {
 
 Color brighten(Color c, [int percent = 30]) {
   var p = percent / 100;
-
   return Color.fromARGB(
       c.alpha,
       c.red + ((255 - c.red) * p).round(),
